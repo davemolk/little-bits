bc0b8d636e7a1ad6fe234580b713a7a3f9bd714e
Dave Molk <davemolk@gmail.com>
2024-11-28 08:01:05 -0700

    refactor pw

--- io_utils.rb
+++ io_utils.rb
@@ -1,6 +1,7 @@
 
 module IoUtils
-  def self.copy_to_clipboard(value)
+  def self.copy_to_clipboard(value, print_value=false)
+    msg = "copied and ready to paste"
     if Gem.win_platform?
       Open3.pop3('clip') do |stdin, _, _, _|
         stdin.puts value
@@ -8,10 +9,10 @@ module IoUtils
     else
       if system("which pbcopy > /dev/null 2>&1")
         IO.popen("pbcopy", "w") { |f| f << value }
-        puts "copied and ready to paste"
+        puts print_value ? "#{value} #{msg}" : msg
       elsif system("which xclip > /dev/null 2>&1")
         IO.popen("xclip -selection clipboard", "w") { |f| f << value }
-        puts "copied and ready to paste"
+        puts print_value ? "#{value} #{msg}" : msg
       else
         puts "no clipboard utility found :/"
         puts value
--- pw.rb
+++ pw.rb
@@ -8,46 +8,55 @@ require './http_utils.rb'
 
 class PW
   DEFAULT_DIR = ".pw"
-  LARGE_WORDLIST = "https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt"
-  SHORT_WORDLIST = "https://www.eff.org/files/2016/09/08/eff_short_wordlist_1.txt"
-  SHORT_WORDLIST2 = "https://www.eff.org/files/2016/09/08/eff_short_wordlist_2_0.txt"
+  WORDLISTS = {
+    "large": "https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt",
+    "short1": "https://www.eff.org/files/2016/09/08/eff_short_wordlist_1.txt",
+    "short2": "https://www.eff.org/files/2016/09/08/eff_short_wordlist_2_0.txt",
+  }
 
   def initialize
     @path = File.join(ENV['HOME'], DEFAULT_DIR)
     FileUtils.mkdir_p(@path) unless Dir.exist?(@path)
-
-    # download files
-    # clean files (and save)
-    # profit
   end
 
-  def get_data(url)
-    
-  end
-
-  def clean_data
-    Dir.chdir(@path)
-    files = Dir.glob("*.txt")
-    files.each do |f|
-      # not doing dice rolls, so change 11111	abacus to abacus
-      data = File.readlines(f).map{ |line| line.sub(/^[\d]*[\t\s]*/, "") }
-      cleaned = data.join("")
-      File.write(f, cleaned)
+  def get_data
+    WORDLISTS.each do |k, v|
+      data = HttpUtils.fetch_data(v)
+      data.gsub!(/[\d\t]*/, "")
+      list_path = File.join(@path, "#{k}.txt")
+      File.write(list_path, data)
     end
   end
 end
 
-def generate(file, word_count, separator)
+def generate(file, word_count, separator, num=false, special=false, capital=false, complex=false)
   words = File.readlines(file).map(&:chomp)
-  chosen_words = []
-  begin
-    word_count.times { chosen_words << words.sample }
-  rescue
-    word_count.times { chosen_words << words.sample(random: SecureRandom) }
+  special_characters = '!@#$%^&*()_+-=[]{}|;:,.<>?/~'.chars
+  capitals = 'ABCDEFGHIJLKMNOPQRSTUVWXYZ'.chars
+  
+  chosen_words = Array.new(word_count) { words.sample(random: SecureRandom) }
+  extras = []
+
+  add_extras = ->(array, condition, extra_pool) do
+    if !condition
+      return array
+    end
+    if !complex
+      extras << extra_pool.sample
+      array
+    else
+      array.map { |word| "#{word}#{extra_pool.sample}" }
+    end
   end
+  
+  chosen_words = add_extras.call(chosen_words, num, (0..9).to_a)
+  chosen_words = add_extras.call(chosen_words, special, special_characters)
+  chosen_words = add_extras.call(chosen_words, capital, capitals)
+
   pw = chosen_words.join(separator)
-  IoUtils.copy_to_clipboard(pw)
-  puts pw
+  pw << extras.join
+
+  IoUtils.copy_to_clipboard(pw, true)
 end
 
 def parse_options
@@ -64,14 +73,25 @@ def parse_options
     opts.on("-l", "--large", "use EFF large wordlist") { options[:word_list] = "large.txt" }
     opts.on("-s", "--short2", "use EFF short wordlist 2 (default is short wordlist 1)") { options[:word_list] = "short2.txt" }
     opts.on("--initialize", "get the EFF lists and get passwording!") { options[:initialize] = true }
+    opts.on("--capital", "include capital letters") { options[:capital] = true }
+    opts.on("--numbers", "include numbers") { options[:num] = true }
+    opts.on("--special", "include special chars") { options[:special] = true }
+    opts.on("--complex", "include number/special char/capital at the end of each word (dependong on which is selected)") { options[:complex] = true }
+    opts.on("--custom CUSTOM", "path to custom wordlist") { |c| options[:custom] = c }
   end.parse!
   options
 end
 
+def init
+  initializer = PW.new()
+  initializer.get_data()
+end
+
 def main
   options = parse_options
-  path = File.join(ENV['HOME'], ".pw", options[:word_list])
-  generate(path, options[:word_count], options[:separator])
+  init unless !options[:initialize]
+  path = options[:custom] ? options[:custom] : File.join(ENV['HOME'], ".pw", options[:word_list])
+  generate(path, options[:word_count], options[:separator], options[:num], options[:special], options[:capital], options[:complex])
 rescue StandardError => e
   warn "error: #{e.message}"
   exit 1
